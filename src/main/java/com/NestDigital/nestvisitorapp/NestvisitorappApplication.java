079babc7702f49567d48d5fd888cd468466076ef
Anand-S-L <anandslperoorkada@gmail.com>
2022-09-12 11:08:25 +0530

    added

---
+++ src/main/java/com/NestDigital/nestvisitorapp/NestvisitorappApplication.java
@@ -0,0 +1,13 @@
+package com.NestDigital.nestvisitorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestvisitorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestvisitorappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NestDigital/nestvisitorapp/NestvisitorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NestDigital.nestvisitorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestvisitorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
