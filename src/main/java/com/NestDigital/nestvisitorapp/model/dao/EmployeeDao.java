5e1717cf59617dad2c7d1ff64ff3b351f56c9b93
Anand-S-L <anandslperoorkada@gmail.com>
2022-09-12 12:23:50 +0530

    added viewallmployee

--- src/main/java/com/NestDigital/nestvisitorapp/model/dao/EmployeeDao.java
+++ src/main/java/com/NestDigital/nestvisitorapp/model/dao/EmployeeDao.java
@@ -1,7 +1,16 @@
 package com.NestDigital.nestvisitorapp.model.dao;
 
 import com.NestDigital.nestvisitorapp.model.EmployeeModel;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
+
 public interface EmployeeDao extends CrudRepository<EmployeeModel,Integer> {
+//    update employee
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE `employees` SET `designation`='?1',`emploeecode`='?2',`name`='?3',`password`='?4',`phoneno`='?5',`username`='?6' WHERE id=?7",nativeQuery = true)
+    void updateemployee(String designation,String employeecode,String name,String password,String phoneno,String username,int id);
 }
